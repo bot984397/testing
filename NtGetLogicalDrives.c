9ca5591bc3b37980b3d7d16a0a221cd22bce5b8f
nightcall489 <nightcall489@gmail.com>
2024-12-30 08:59:33 +0100

    fixup v2

--- NtGetLogicalDrives.c
+++ NtGetLogicalDrives.c
@@ -2,9 +2,6 @@
 #include <winternl.h>
 #include <stdio.h>
 
-typedef int PROCESSINFOCLASS;
-#define ProcessDeviceMap 0x17
-
 typedef NTSTATUS(NTAPI* NtQueryInformationProcess_t) (
    HANDLE           ProcessHandle,
    PROCESSINFOCLASS ProcessInformationClass,
@@ -35,7 +32,7 @@ int main(void)
       = (NtQueryInformationProcess_t)GetProcAddress(LoadLibraryA("ntdll.dll"), "NtQueryInformationProcess");
 
    PROCESS_DEVICEMAP_INFORMATION out = { 0 };
-   NTSTATUS ret = NtQueryInformationProcess(-1, ProcessDeviceMap, &out, 0x24, 0);
+   NTSTATUS ret = NtQueryInformationProcess((HANDLE)-1, 0x17, &out, 0x24, 0);
 
    return 0;
 }
