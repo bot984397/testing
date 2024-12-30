21071a975cdbf81927d3b563b4894748b5f7cc7d
nightcall489 <nightcall489@gmail.com>
2024-12-30 08:30:10 +0100

    native GetLogicalDrives

---
+++ NtGetLogicalDrives.c
@@ -0,0 +1,40 @@
+#include <Windows.h>
+#include <winternl.h>
+#include <stdio.h>
+
+typedef int PROCESSINFOCLASS;
+
+typedef NTSTATUS(NTAPI* NtQueryInformationProcess_t) (
+   HANDLE           ProcessHandle,
+   PROCESSINFOCLASS ProcessInformationClass,
+   PVOID            ProcessInformation,
+   ULONG            ProcessInformationLength,
+   PULONG           ReturnLength
+);
+
+typedef struct
+{
+   union
+   {
+      struct
+      {
+         HANDLE DirectoryHandle;
+      } Set;
+      struct
+      {
+         ULONG DriveMap;
+         UCHAR DriveType[32];
+      } Query;
+   };
+} PROCESS_DEVICEMAP_INFORMATION;
+
+int main(void)
+{
+   NtQueryInformationProcess_t NtQueryInformationProcess
+      = (NtQueryInformationProcess_t)GetProcAddress(LoadLibraryA("ntdll.dll"), "NtQueryInformationProcess");
+
+   PROCESS_DEVICEMAP_INFORMATION out = { 0 };
+   NTSTATUS ret = NtQueryInformationProcess(-1, 0x17, &out, 0x24, 0);
+
+   return 0;
+}
